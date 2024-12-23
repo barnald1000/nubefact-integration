edf5144a1f918bbc3a4636d12db245c3a10bbc3e
Enea Dhack <enea@vaened.dev>
2024-12-23 12:49:10 -0500

    feat: add Currency enum
    This commit adds a new enum class `Currency` with EUR, USD, and PEN currency options to facilitate currency handling in the application.🚀

---
+++ src/Keywords/Currency.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Savia\NubeFact;
+
+enum Currency: int
+{
+    case EUR = 3;
+
+    case USD = 2;
+
+    case PEN = 1;
+}
