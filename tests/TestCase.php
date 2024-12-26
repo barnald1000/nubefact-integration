9f4cb2173575ebba2a93f45e09b6ec4b7837d8c1
Enea Dhack <enea@vaened.dev>
2024-12-26 13:08:04 -0500

    feat: Add initial PHPUnit configuration
    Added a new file phpunit.xml to configure PHPUnit with basic settings and test suite structure for the project.
    
    This commit establishes the foundation for running PHPUnit tests seamlessly within the project structure. 🧪🚀

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Savia\NubeFact\Tests;
+
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+
+abstract class TestCase extends PhpUnitTestCase
+{
+}
