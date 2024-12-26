b27df6ea5fb38e8ff9b7e7f25d529e28acc07151
Enea Dhack <enea@vaened.dev>
2024-12-26 16:09:01 -0500

    feat: Add GuzzleNubeFactService implementation
    This commit adds the GuzzleNubeFactService class, which implements the NubeFactService interface. It provides methods to call API operations using Guzzle HTTP client.

--- src/NubeFactService.php
+++ src/NubeFactService.php
@@ -8,48 +8,10 @@ declare(strict_types=1);
 
 namespace Savia\NubeFact;
 
-use GuzzleHttp\Client as GuzzleClient;
-use GuzzleHttp\HandlerStack;
 use Psr\Http\Message\ResponseInterface;
 use Savia\NubeFact\Keywords\Operation;
 
-use function array_merge;
-use function sprintf;
-
-final class NubeFactService
+interface NubeFactService
 {
-    private static GuzzleClient $client;
-
-    public function __construct(string $baseURI, string $apiToken)
-    {
-        self::$client = self::createClient($baseURI, $apiToken);
-    }
-
-    public function call(Operation $operation, array $data): ResponseInterface
-    {
-        return $this->client()->post('', self::request($operation, with: $data));
-    }
-
-    private static function client(): GuzzleClient
-    {
-        return self::$client;
-    }
-
-    private static function createClient(string $baseURI, string $apiToken): GuzzleClient
-    {
-        return new GuzzleClient([
-            'base_uri' => $baseURI,
-            'handler'  => HandlerStack::create(),
-            'headers'  => [
-                'Content-Type'  => 'application/json',
-                'Accept'        => 'application/json',
-                'Authorization' => sprintf("Token token=%s", $apiToken),
-            ],
-        ]);
-    }
-
-    private static function request(Operation $operation, array $with): array
-    {
-        return array_merge(['operacion' => $operation->value], $with);
-    }
+    public function call(Operation $operation, array $data): ResponseInterface;
 }
