fa017a4b70e13ed2b035298698ae39aaad61a8ea
Enea Dhack <enea@vaened.dev>
2024-12-26 12:52:35 -0500

    feat: add TransactionProcessor class
    This commit adds a new TransactionProcessor class responsible for processing financial transactions by sending them and logging the responses. It includes methods to build success and error responses based on the transaction outcome.🚀

---
+++ src/TransactionProcessor.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Savia\NubeFact;
+
+use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
+use Savia\NubeFact\Contracts\FinancialTransaction;
+use Savia\NubeFact\Responses\Error;
+use Savia\NubeFact\Responses\Response;
+use Savia\NubeFact\Responses\Success;
+use Vaened\Support\Types\ArrayList;
+
+use function json_decode;
+
+final readonly class TransactionProcessor
+{
+    public function __construct(
+        private Sender            $sender,
+        private ?LoggerRepository $logger = null
+    )
+    {
+    }
+
+    public function process(FinancialTransactions $transactions): ArrayList
+    {
+        return $transactions->map($this->sendAndLog());
+    }
+
+    private function sendAndLog(): callable
+    {
+        return function (FinancialTransaction $transaction): Response {
+            $response = null;
+
+            try {
+                $response = $this->sender->send($transaction);
+                return $this->buildSuccessResponseFor($transaction, $response);
+            } catch (RequestException $exception) {
+                return $this->buildErrorResponseFor($transaction, $exception, $response);
+            }
+        };
+    }
+
+    private function buildSuccessResponseFor(
+        FinancialTransaction $transaction,
+        ResponseInterface    $response
+    ): Success
+    {
+        $response = new Success($transaction, json_decode($response->getBody()->getContents(), true));
+        $this->logger?->log($response);
+
+        return $response;
+    }
+
+    private function buildErrorResponseFor(
+        FinancialTransaction $transaction,
+        RequestException     $exception,
+        ResponseInterface    $response
+    ): Error
+    {
+        $response = new Error($transaction, $exception->getMessage(), json_decode($response->getBody()->getContents(), true));
+        $this->logger?->log($response);
+
+        return $response;
+    }
+}
