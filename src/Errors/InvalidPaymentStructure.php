c80133aa283be6a299f19e9ae4bebc727d9f7d8d
Enea Dhack <enea@vaened.dev>
2024-12-26 12:49:43 -0500

    feat: Add InvalidPaymentStructure and NubeFactErrorException classes
    Added new classes InvalidPaymentStructure and NubeFactErrorException to handle specific errors in the Savia NubeFact application. These classes will improve error handling and make the codebase more robust. 🚀

---
+++ src/Errors/InvalidPaymentStructure.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Savia\NubeFact\Errors;
+
+final class InvalidPaymentStructure extends NubeFactErrorException
+{
+}
---
+++ src/Errors/NubeFactErrorException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Savia\NubeFact\Errors;
+
+use RuntimeException;
+
+abstract class NubeFactErrorException extends RuntimeException
+{
+}
